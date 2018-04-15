b38292e0a8ad5a52453c4d6d7bd0f8ef16603a96
Aaron Martins <me@aaronmartins.com>
2018-04-15 12:20:13 -0700

    Add mongo help

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ const argv = minimist(process.argv.slice(2));
 const { MongoClient } = mongodb;
 
 const connectionString = argv.dbStr || `mongodb://localhost:27017/${constants.DB_NAME}`;
-const suggestedMongoCmd = 'docker run --name mongo -p 27017:27017 -d mongo:latest';
+const suggestedMongoCmd = 'docker run --name pwalk -p 27017:27017 -v /my/own/datadir:/data/db -d mongo:latest';
 
 if (!argv.src) {
     err('You must specify a source, use "--src=/my/dir"');
@@ -29,7 +29,7 @@ async function main() {
     try {
         dbClient = await MongoClient.connect(connectionString);
     } catch (e) {
-        err(`Couldn't connect to ${connectionString}. Perhaps you need to run "${suggestedMongoCmd}"?`);
+        err(`Couldn't connect to ${connectionString}. Perhaps you need to run something like 'npm run createMongo'? See https://hub.docker.com/_/mongo/ for more.`);
         exit();
     }
 
